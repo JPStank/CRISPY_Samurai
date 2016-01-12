eba379c92f64ab6069e4f80deeeef2ae6a6e244e
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2016-01-12 14:20:50 -0500

    Sam: starting on sword collision functions

---
+++ Unity Base Project/Assets/Scripts/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour 
+{
+	public PuppetScript owner;
+	public GameObject sparkEffect;
+
+	//damage and other goodies
+
+	// Use this for initialization
+	//void Start () 
+	//{
+	//
+	//}
+	
+	// Update is called once per frame
+	//void Update () 
+	//{
+	//
+	//}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.transform.tag == "Weapon")
+		{
+			Instantiate(sparkEffect, other.contacts[0].point, Quaternion.identity);
+		}
+	}
+
+	PuppetScript.State GetState()
+	{
+		return owner.curState;
+	}
+}
