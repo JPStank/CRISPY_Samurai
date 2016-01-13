67e4c62469288916325337814dc8d8b4bec7f36b
Logan <lmstallman@fullsail.edu>
2016-01-13 09:36:52 -0500

    Debug scene updated with meat. BloodyBag updated with cost.

--- Unity Base Project/Assets/Animations/Josh's temp files/BloodyBag.cs
+++ Unity Base Project/Assets/Animations/Josh's temp files/BloodyBag.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BloodyBag : MonoBehaviour 
 {
     public GameObject bloodEffect;
+	public float hitCost = 1.0f;
 
 	// Use this for initialization
 	void Start () 
