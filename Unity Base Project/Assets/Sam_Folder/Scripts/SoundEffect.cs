60f6f83d4aa133a3f70ed854901933f5575a2a59
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2016-01-13 14:24:20 -0500

    Sam: created sound effect script and prefabs

---
+++ Unity Base Project/Assets/Sam_Folder/Scripts/SoundEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffect : MonoBehaviour
+{
+	public AudioSource sfx;
+	public AudioSource[] variety;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sfx = variety[Random.Range(0, variety.Length)];
+		sfx.Play();
+		Destroy(gameObject, 1.0f);
+	}
+}
