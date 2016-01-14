4f238608e9b4abd72aff730ee0f41e4d9803537d
Steven Quast <smquast@gmail.com>
2016-01-14 15:19:50 -0500

    Added a bunch of Harry Potter SLASH fics

--- Unity Base Project/Assets/Scripts/Action.cs
+++ Unity Base Project/Assets/Scripts/Action.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class Action : MonoBehaviour
 {
-    bool behaving = false;
+    protected bool behaving = false;
+    protected PuppetScript puppet;
 	// Use this for initialization
 	void Start ()
 	{
-		
+        puppet = GetComponent<PuppetScript>();
 	}
 	
 	// Update is called once per frame
