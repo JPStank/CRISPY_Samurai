4184617b1efd8a2d026f14516c893be7e26384a1
Dakota Cardillo <Dcardillo@fullsail.edu>
2016-01-13 19:57:08 -0500

    Implemented Balance UI, New Debug info

--- Unity Base Project/Assets/Animations/Josh's temp files/BloodyBag.cs
+++ Unity Base Project/Assets/Animations/Josh's temp files/BloodyBag.cs
@@ -5,6 +5,7 @@ public class BloodyBag : MonoBehaviour
 {
     public GameObject bloodEffect;
 	public float hitCost = 1.0f;
+	public GameObject player;
 
 	// Use this for initialization
 	void Start () 
@@ -26,6 +27,20 @@ public class BloodyBag : MonoBehaviour
 			{
 				//temp.owner.canHit = false;
 				Instantiate(bloodEffect, other.contacts[0].point, bloodEffect.transform.rotation);
+
+				if (player)
+				{
+					player.GetComponent<PuppetScript>().curBalance -= (hitCost * 5.0f);
+					if (player.GetComponent<PuppetScript>().curBalance < 0.0f)
+						player.GetComponent<PuppetScript>().curBalance = 0.0f;
+				}
+
+				if (temp.owner.tag == "Player")
+				{
+					temp.owner.GetComponent<PuppetScript>().curBalance += (hitCost * 3.0f);
+					if (temp.owner.GetComponent<PuppetScript>().curBalance > temp.owner.GetComponent<PuppetScript>().maxBalance)
+						temp.owner.GetComponent<PuppetScript>().curBalance = temp.owner.GetComponent<PuppetScript>().maxBalance;
+				}
 			}
 
 			//other.gameObject.GetComponent<>();
---
+++ Unity Base Project/Assets/Scripts/GUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUI : MonoBehaviour
+{
+	public Image guardLeft, guardRight, guardTop, balance;
+	private PuppetScript player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PuppetScript>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (player.curState == PuppetScript.State.GRD_LEFT)
+			guardLeft.color = Color.red;
+		else
+			guardLeft.color = Color.white;
+
+		if (player.curState == PuppetScript.State.GRD_TOP)
+			guardTop.color = Color.red;
+		else
+			guardTop.color = Color.white;
+
+		if (player.curState == PuppetScript.State.GRD_RIGHT)
+			guardRight.color = Color.red;
+		else
+			guardRight.color = Color.white;
+
+		balance.fillAmount = player.curBalance / player.maxBalance;
+	}
+}
