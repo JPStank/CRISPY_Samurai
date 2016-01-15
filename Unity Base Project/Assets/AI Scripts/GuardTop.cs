0ac7b2ccaa710dea59d1688eb1c9b0a4f42f384e
Steven Quast <smquast@gmail.com>
2016-01-15 15:59:19 -0500

    Added Guard Actions

---
+++ Unity Base Project/Assets/AI Scripts/GuardTop.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardTop : Action
+{
+	public float GuardTimerMax = 1.0f;
+	float timer = 0.0f;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public override COMPLETION_STATE Execute()
+	{
+		if (!behaving)
+		{
+			behaving = true;
+			puppet.GuardUpwards();
+		}
+
+		timer += Time.deltaTime;
+		if (timer >= GuardTimerMax)
+		{
+			animation.Play("Idle");
+			puppet.ChangeState(PuppetScript.State.IDLE);
+			behaving = false;
+			return COMPLETION_STATE.COMPLETE;
+		}
+
+		return COMPLETION_STATE.IN_PROGRESS;
+	}
+}
