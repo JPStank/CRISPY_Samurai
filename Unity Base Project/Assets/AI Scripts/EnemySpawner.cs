bfe34934ae54c788dde0f685736e511c55ad4281
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2016-01-17 14:24:11 -0500

    CH-CH-CHANGES!

---
+++ Unity Base Project/Assets/AI Scripts/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour 
+{
+    public GameObject spawnPoint = null;
+
+    public GameObject[] enemies;
+
+    public GameObject currEnemy;
+
+    int index = 0; // use this to browse the list of enemies
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (enemies.Length == 0)
+        {
+            Debug.LogWarning("YOU MUST SET ENEMIES IDIOT!!");
+        }
+        if (spawnPoint == null)
+        {
+            Debug.LogWarning("YOU MUST SET SPAWN LOCATION IDIOT!!");
+        }
+        //currEnemy = enemies[index];
+        currEnemy = (GameObject)Instantiate(enemies[index], spawnPoint.transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        PuppetScript p = currEnemy.GetComponent<PuppetScript>();
+
+        if (p && p.curBalance <= 0.0f)
+        {
+            //p.ChangeState(PuppetScript.State.DEAD);
+            Destroy(currEnemy, currEnemy.GetComponent<Animation>()["Dead"].length + 0.5f);
+            currEnemy = null;
+
+            index++;
+            if (index >= enemies.Length)
+            {
+                index = 0;
+            }
+
+            currEnemy = (GameObject)Instantiate(enemies[index], spawnPoint.transform.position, Quaternion.identity);
+        }
+	}
+}
