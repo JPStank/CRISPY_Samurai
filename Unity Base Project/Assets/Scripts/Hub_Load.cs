92ac222defe8a41b00b56664ff5f62d71dc4d0a4
Dakota Cardillo <Dcardillo@fullsail.edu>
2016-01-18 14:51:45 -0500

    Made a hub scene, individual scenes to be added

---
+++ Unity Base Project/Assets/Scripts/Hub_Load.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hub_Load : MonoBehaviour
+{
+	public string levelName;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter()
+	{
+		Application.LoadLevel(levelName);
+	}
+}
