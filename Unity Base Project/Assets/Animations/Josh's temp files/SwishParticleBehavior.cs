3b09fe39bdd96c754b0c3c85f90314e2dce48415
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2016-01-19 16:21:07 -0500

    SWISH IS CONTROLED BY ANIMATION PUNK

--- Unity Base Project/Assets/Animations/Josh's temp files/SwishParticleBehavior.cs
+++ Unity Base Project/Assets/Animations/Josh's temp files/SwishParticleBehavior.cs
@@ -3,39 +3,25 @@ using System.Collections;
 
 public class SwishParticleBehavior : MonoBehaviour 
 {
-    public ParticleSystem ps;
-
-    private Vector3 originalRotation;
-
-    public Vector3 endRotation;
-
-    public float duration = 0.1f;
-
-    private float timer = 0.0f;
+    public TrailRenderer trail;
 
 	void Start () 
     {
-	    if (ps == null)
+        if (trail == null)
         {
-            ps = GetComponentInChildren<ParticleSystem>();
-            if (ps == null)
-            {
-                Debug.LogError("Must have Particle System added as child");
-            }
+            Debug.LogError("YOU MUST SET THE TRAIL IDIOT!!");
         }
 
-        originalRotation = transform.eulerAngles;
+        trail.enabled = false;
 	}
 	
-	void Update() 
+    public void ActivateTrail()
     {
+        trail.enabled = true;
+    }
 
-        transform.Rotate(Vector3.right, -360.0f * Time.deltaTime);
-        //timer += Time.deltaTime;
-        //if (timer >= duration)
-        //{
-        //    Destroy(gameObject);
-        //}
-        //transform.rotation = Quaternion.Slerp(Quaternion.Euler(originalRotation), Quaternion.Euler(endRotation), timer / duration);
-	}
+    public void DisableTrail()
+    {
+        trail.enabled = false;
+    }
 }
