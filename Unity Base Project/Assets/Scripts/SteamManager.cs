a56760cb3f528978829775c52841d5439baf97b1
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2016-01-19 09:21:19 -0500

    RAWR

--- Unity Base Project/Assets/Scripts/SteamManager.cs
+++ Unity Base Project/Assets/Scripts/SteamManager.cs
@@ -10,37 +10,24 @@ public class SteamManager : MonoBehaviour
 
     public bool isAI = true;
     public Vector3 test;
-    private Vector3 ea; // cause I(josh) is lazy
 
     private Vector3 leftOffset = new Vector3(0.0f, 90.0f, 0.0f);
     private Vector3 rightOffset = new Vector3(0.0f, 270.0f, 0.0f);
     private Vector3 topOffset = new Vector3(270.0f, 0.0f, 0.0f);
 
-	// Use this for initialization
-	void Start () 
-    {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        ea = transform.eulerAngles;
-	}
-
     void SpawnTop()
     {
         if (isAI)
-            Instantiate(steamEffect, topSpawn.transform.position, Quaternion.Euler(ea + topOffset));
+            Instantiate(steamEffect, topSpawn.transform.position, Quaternion.Euler(transform.eulerAngles + topOffset));
     }
     void SpawnLeft()
     {
         if (isAI)
-            Instantiate(steamEffect, leftSpawn.transform.position, Quaternion.Euler(ea + leftOffset));
+            Instantiate(steamEffect, leftSpawn.transform.position, Quaternion.Euler(transform.eulerAngles + leftOffset));
     }
     void SpawnRight()
     {
         if (isAI)
-            Instantiate(steamEffect, rightSpawn.transform.position, Quaternion.Euler(ea + rightOffset));
+            Instantiate(steamEffect, rightSpawn.transform.position, Quaternion.Euler(transform.eulerAngles + rightOffset));
     }
 }
