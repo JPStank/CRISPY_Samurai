fe63c69a24c69b37d8e7191c5d5c4e386deecc25
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2016-01-22 14:15:22 -0500

    MOAR ENEMIES

--- Unity Base Project/Assets/AI Scripts/EnemySpawner.cs
+++ Unity Base Project/Assets/AI Scripts/EnemySpawner.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawner : MonoBehaviour 
 {
     public GameObject spawnPoint = null;
+    public bool singleSpawn = false;
 
+    [Header("Spawn Points and Enemies must be same size")]
+    public GameObject[] spawnPoints = new GameObject[3];
     public GameObject[] enemies;
+    
+    private List<GameObject> enemiesInScene;
 
     public GameObject currEnemy;
 
@@ -18,15 +24,17 @@ public class EnemySpawner : MonoBehaviour
     private bool waiting = false;
 
     int index = 0; // use this to browse the list of enemies
+    List<int> toRemove = new List<int>(); // things to check for removal
 
 	// Use this for initialization
 	void Start () 
     {
+        toRemove.Clear(); // ensure this is empty for later logic
         if (enemies.Length == 0)
         {
             Debug.LogWarning("YOU MUST SET ENEMIES IDIOT!!");
         }
-        if (spawnPoint == null)
+        if (spawnPoint == null && singleSpawn)
         {
             Debug.LogWarning("YOU MUST SET SPAWN POINT IDIOT!!");
         }
@@ -34,15 +42,84 @@ public class EnemySpawner : MonoBehaviour
         {
             Debug.LogWarning("I NEED DOOR FRIENDS PLS!!");
         }
-        //currEnemy = enemies[index];
-        currEnemy = (GameObject)Instantiate(enemies[index], spawnPoint.transform.position, Quaternion.identity);
-
+        if (singleSpawn) // spawn first enemy
+            currEnemy = (GameObject)Instantiate(enemies[index], spawnPoint.transform.position, Quaternion.identity);
+        //else // spawn all of them
+        //{
+        //    if (enemies.Length == 0)
+        //    {
+        //        Debug.LogError("MULTI SPAWN ENABLED BUT NO ENEMIES GIVEN");
+        //    }
+        //    enemiesInScene = new GameObject[enemies.Length];
+        //    for (int i = 0; i < enemies.Length; ++i)
+        //    {
+        //        enemiesInScene[i] = (GameObject)Instantiate(enemies[i], spawnPoints[i].transform.position, Quaternion.identity);
+        //    }
+        //}
 	}
-	
+	public void SpawnAll()
+    {
+        killsRequired = enemies.Length;
+        if (enemies.Length == 0)
+        {
+            Debug.LogError("MULTI SPAWN ENABLED BUT NO ENEMIES GIVEN");
+        }
+        if (enemies.Length != spawnPoints.Length)
+        {
+            Debug.LogError("SPAWN POINT NEEDED FOR EACH ENEMY");
+        }
+        enemiesInScene = new List<GameObject>();
+        for (int i = 0; i < enemies.Length; ++i)
+        {
+            enemiesInScene.Add((GameObject)Instantiate(enemies[i], spawnPoints[i].transform.position, Quaternion.identity));
+        }
+
+    }
 	// Update is called once per frame
 	void Update () 
     {
-        if (currEnemy)
+        // this will check the status of all enemies
+        if (singleSpawn == false && enemiesInScene != null)
+        {
+            for (int i = 0; i < enemiesInScene.Count; ++i)
+            {
+                PuppetScript p = enemiesInScene[i].GetComponent<PuppetScript>();
+
+                if (p && p.curTallys <= 0.0f)
+                {
+                    Destroy(enemiesInScene[i], enemiesInScene[i].GetComponent<Animation>()["Death"].length + 0.5f);
+                    toRemove.Add(i); // queue for removal
+
+                    killCount++;
+                    if (killCount >= killsRequired)
+                    {
+                        if (doorFriends)
+                            doorFriends.MoveTheDoorsPls(false);
+                    }
+                }
+            }
+
+            // if things need to go, update list
+            if (toRemove.Count > 0)
+            {
+                int remainingEnemies = enemiesInScene.Count - toRemove.Count;
+                if (remainingEnemies <= 0) // all are dead
+                {
+                    enemiesInScene.Clear();
+                    enemiesInScene = null;
+                    return; // stop now
+                }
+
+                foreach(int kill in toRemove)
+                {
+                    enemiesInScene.RemoveAt(kill);
+                }
+
+                toRemove.Clear();
+            }
+        }
+        // this governs single file spawner
+        else if (singleSpawn && currEnemy)
         {
             PuppetScript p = currEnemy.GetComponent<PuppetScript>();
 
@@ -79,6 +156,8 @@ public class EnemySpawner : MonoBehaviour
                 currEnemy = (GameObject)Instantiate(enemies[index], spawnPoint.transform.position, Quaternion.identity);
             }
         }
+
+
 
 	}
 }
