f8bf2164116e3a8ba0bf40cbed115d1f6e3c8f89
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2016-01-26 14:35:34 -0500

    BACON AND YUKON BITCHES!

---
+++ Unity Base Project/Assets/Sam_Folder/Scripts/SplashScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SplashScript : MonoBehaviour
+{
+	public float timer;
+	float initTime;
+	public GameObject publisher, studio;
+
+	// Use this for initialization
+	void Start () 
+	{
+		initTime = timer;
+		studio.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+		if(timer <= (initTime * 0.5f))
+		{
+			publisher.SetActive(false);
+			studio.SetActive(true);
+		}
+		if(timer <= 0.0f)
+		{
+			Application.LoadLevel("Menu_Scene");
+		}
+	}
+}
